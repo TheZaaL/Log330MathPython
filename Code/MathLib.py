f76a63246bd061ebddfc29371bc7dce4f6618d7a
DESKTOP-C9O708V\kevin <kevinleb94@hotmail.com>
2018-03-12 20:05:37 -0400

    TP3 almost finished

--- Code/MathLib.py
+++ Code/MathLib.py
@@ -11,6 +11,14 @@ def sum(list):
 
   return output
 
+def product(list_1, list_2):
+  output = []
+
+  for i in range(0, len(list_1)):
+    output.append(list_1[i] * list_2[i])
+  
+  return output
+
 def sum_distance_squared(list):
   sum_distance = 0
   average_list = average(list)
@@ -25,3 +33,20 @@ def variance(list):
 
 def ecartType(list):
   return sqrt(variance(list))
+
+def correlation(list_x, list_y):
+  n = len(list_x)
+
+  list_xx = product(list_x, list_x)
+  list_xy = product(list_x, list_y)
+  list_yy = product(list_y, list_y)
+
+  sum_x = sum(list_x)
+  sum_y = sum(list_y)
+
+  dividend = ( n * sum(list_xy) ) - ( sum_x * sum_y )
+
+  divisor = ( (n * sum(list_xx)) - (sum_x * sum_x) ) * ( (n * sum(list_yy)) - (sum_y * sum_y) )
+  divisor = sqrt(divisor)
+
+  return dividend/divisor
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Code.IOLib import read_file
-from Code.MathLib import average, ecartType, variance
+from Code.MathLib import average, ecartType, variance, correlation
 
 test_data = read_file("Data/data_ecart_type.csv")
 
@@ -8,3 +8,9 @@ print('Moyenne : %.2f' % average(test_data[0]))
 print('Variance : %.4f' % variance(test_data[0]))
 
 print('Écart type : %.2f' % ecartType(test_data[0]))
+
+test_data_correlation = read_file("Data/data_cor.csv")
+
+cor = correlation(test_data_correlation[0], test_data_correlation[1])
+
+print('Correlation : %.8f' % cor)
