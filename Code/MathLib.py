914c0e4cd2c6758c5905ae3b30d5831678b34ec6
DESKTOP-C9O708V\kevin <kevinleb94@hotmail.com>
2018-03-22 20:04:28 -0400

    Regression done

--- Code/MathLib.py
+++ Code/MathLib.py
@@ -35,6 +35,9 @@ def ecartType(list):
   return sqrt(variance(list))
 
 def correlation(list_x, list_y):
+  if len(list_x) != len(list_y):
+    raise TypeError("Lists are of different lenghts")
+
   n = len(list_x)
 
   list_xx = product(list_x, list_x)
@@ -49,4 +52,27 @@ def correlation(list_x, list_y):
   divisor = ( (n * sum(list_xx)) - (sum_x * sum_x) ) * ( (n * sum(list_yy)) - (sum_y * sum_y) )
   divisor = sqrt(divisor)
 
-  return dividend/divisor
+  return dividend / divisor
+
+def regression_b1(list_x, list_y):
+  if len(list_x) != len(list_y):
+    raise TypeError("Lists are of different lenghts")
+
+  n = len(list_x)
+  
+  average_x = average(list_x)
+  average_y = average(list_y)
+
+  list_xy = product(list_x, list_y)
+  list_xx = product(list_x, list_x)
+
+  dividend = sum(list_xy) - (n * average_x * average_y)
+
+  divisor = sum(list_xx) - (n * average_x * average_x)
+
+  return dividend / divisor
+
+def regression_b0 (list_x, list_y) :
+  regression_y = regression_b1(list_x, list_y) * average(list_x)
+
+  return average(list_y) - regression_y
