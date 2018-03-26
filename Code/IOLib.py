08719c94f85fb1e61b7209848b9356423fee5901
DESKTOP-C9O708V\kevin <kevinleb94@hotmail.com>
2018-03-26 19:24:09 -0400

    TP5 code done

--- Code/IOLib.py
+++ Code/IOLib.py
@@ -3,9 +3,10 @@ import csv
 def read_file(file_path):
   data = []
   with open(file_path) as file_data:
-    reader = csv.reader(file_data, delimiter=';')
+    reader = csv.reader(file_data, delimiter=',')
 
-    # Skip first line
+    # Skip first and second lines
+    next(reader)
     next(reader)
 
     # Invert column and rows for better manipulation
@@ -13,13 +14,19 @@ def read_file(file_path):
 
     for col in range(0, len(first_data)):
       data.append([])
-      data[col].append(float(first_data[col].replace(',', '.')))
+      try:
+        data[col].append(float(first_data[col]))
+      except ValueError:
+        data[col].append(first_data[col])
 
     for row in reader:
       for col, col_data in enumerate(row):
         data.append([])
 
         # Convert to float
-        data[col].append(float(col_data.replace(',', '.')))
+        try:
+          data[col].append(float(col_data))
+        except ValueError:
+          data[col].append(col_data)
     
     return data
--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 from Code.IOLib import read_file
 from Code.MathLib import average, ecartType, variance, correlation, correlation_text, regression_b1, regression_b0
 
-test_data = read_file("Data/data_ecart_type.csv")
+test_data = read_file("Data/tp5_data.csv")
 
-print('Moyenne : %.2f' % average(test_data[0]))
+cor_semaine_1 = correlation(test_data[1], test_data[7])
+cor_semaine_2 = correlation(test_data[2], test_data[7])
+cor_semaine_3 = correlation(test_data[3], test_data[7])
+cor_semaine_4 = correlation(test_data[4], test_data[7])
+cor_semaine_5 = correlation(test_data[5], test_data[7])
+cor_semaine_6 = correlation(test_data[6], test_data[7])
 
-print('Variance : %.4f' % variance(test_data[0]))
-
-print('Écart type : %.2f' % ecartType(test_data[0]))
-
-test_data_correlation = read_file("Data/data_cor.csv")
-
-cor = correlation(test_data_correlation[0], test_data_correlation[1])
-
-print('Correlation : %.8f' % cor)
-
-print(correlation_text(cor))
-
-test_data_regression = read_file("Data/data_regression.csv")
-
-b1 = regression_b1(test_data_regression[0], test_data_regression[1])
-
-print("Regression b1 : %.4f" % b1)
-
-b0 = regression_b0(test_data_regression[0], test_data_regression[1], b1)
-
-print("Regression b0 : %.4f" % b0)
+print("Week 1: this week has a " + correlation_text(cor_semaine_1) + " with a correlation of %.2f" % cor_semaine_1)
+print("Week 2: this week has a " + correlation_text(cor_semaine_2) + " with a correlation of %.2f" % cor_semaine_2)
+print("Week 3: this week has a " + correlation_text(cor_semaine_3) + " with a correlation of %.2f" % cor_semaine_3)
+print("Week 4: this week has a " + correlation_text(cor_semaine_4) + " with a correlation of %.2f" % cor_semaine_4)
+print("Week 5: this week has a " + correlation_text(cor_semaine_5) + " with a correlation of %.2f" % cor_semaine_5)
+print("Week 6: this week has a " + correlation_text(cor_semaine_6) + " with a correlation of %.2f" % cor_semaine_6)
